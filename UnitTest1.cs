7c315d6e4ed7c48bd1edd14f22684e9be240a377
JoeEazzy247 <67006267+JoeEazzy247@users.noreply.github.com>
2025-03-02 18:57:56 +0000

    First commit

---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test;
+
+public class Tests
+{
+    IWebDriver driver;
+    [SetUp]
+    public void Setup()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://demoqa.com");
+        driver.Manage().Window.Maximize();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        
+    }
+
+    [TearDown]
+    public void ShutDownBrowser()
+    {
+        Thread.Sleep(5000);
+        driver.Quit();
+    }
+}
